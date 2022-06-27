379dd6294615e93e8d624f9fc50fca3f0e396d60
Angel Dominguez <angeldomp49@gmail.com>
2022-06-27 12:16:53 -0500

    new features

--- README.md
+++ README.md
@@ -9,3 +9,11 @@
     get(): any
 
 reads the ./tezcatl.config.json file and returns the object
+
+    read: (filename: string) : any
+
+Same as get but reads a custom file
+
+    item(key: string): string 
+
+get the value of a key in the settings object or empty string ""
--- src/Settings.ts
+++ src/Settings.ts
@@ -3,8 +3,31 @@ import {CONFIG_FILE} from "@makechtec/tezcatl-constants";
 import {cwd} from "node:process";
 
 export const Settings = {
+
     get: () => {
         const text = Reader.read(cwd() + "/" +CONFIG_FILE);
         return JSON.parse(text);
+    },
+
+    read: (filename: string) : any => {
+        const text = Reader.read(filename);
+        return JSON.parse(text);
+    },
+
+    item(key: string): string {
+        try{
+            const item = Settings.get()[key];
+            if(item === undefined || item === null){
+                throw new Error("Item not found");
+            }
+
+            return item;
+        }
+        catch(e){
+            console.info("INFO: la propiedad " + key + " no existe en el archivo de configuración");
+            return "";
+        }
+
     }
+
 };
